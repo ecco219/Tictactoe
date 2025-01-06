c43f6ddccec52778200a64da81b345947e5baf27
User <user@example.com>
2025-01-06 13:14:41 -0600

    Update Replit configuration to ensure Python availability

--- replit.nix
+++ replit.nix
@@ -1,10 +1,14 @@
 { pkgs }: {
     deps = [
+        pkgs.python3
         pkgs.python310
+        pkgs.python39
+        pkgs.python38
+        pkgs.python
+        pkgs.pythonFull
         pkgs.python310Packages.http-server
         pkgs.nodejs-16_x
         pkgs.nodePackages.http-server
-        pkgs.python3
         pkgs.python3Packages.pip
     ];
     env = {
@@ -12,7 +16,7 @@
             # Needed for pandas / numpy
             pkgs.stdenv.cc.cc.lib
         ];
-        PYTHONBIN = "${pkgs.python310}/bin/python3.10";
+        PYTHONBIN = "${pkgs.python3}/bin/python3";
         PYTHONBIN3 = "${pkgs.python3}/bin/python3";
         LANG = "en_US.UTF-8";
         STDERREDBIN = "${pkgs.stdenv.cc.cc}/bin/stderred";
