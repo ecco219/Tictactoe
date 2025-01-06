d2258c27f607a713dd65bfa9dbd2b667ebf68a6f
User <user@example.com>
2025-01-06 13:15:19 -0600

    Update to use Node.js http-server for Replit

--- replit.nix
+++ replit.nix
@@ -1,25 +1,10 @@
 { pkgs }: {
     deps = [
-        pkgs.python3
-        pkgs.python310
-        pkgs.python39
-        pkgs.python38
-        pkgs.python
-        pkgs.pythonFull
-        pkgs.python310Packages.http-server
         pkgs.nodejs-16_x
         pkgs.nodePackages.http-server
-        pkgs.python3Packages.pip
     ];
     env = {
-        PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-            # Needed for pandas / numpy
-            pkgs.stdenv.cc.cc.lib
-        ];
-        PYTHONBIN = "${pkgs.python3}/bin/python3";
-        PYTHONBIN3 = "${pkgs.python3}/bin/python3";
-        LANG = "en_US.UTF-8";
-        STDERREDBIN = "${pkgs.stdenv.cc.cc}/bin/stderred";
         NODEJS_VERSION = "16.x";
+        LANG = "en_US.UTF-8";
     };
 }
